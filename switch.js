78f37daf9fc836d2381553ce5d28092161de7949
yusuf ridwan <horiyorrmi72@gmal.com>
2020-11-11 15:35:22 +0100

    the switch

---
+++ switch.js
@@ -0,0 +1,16 @@
+let a = 2+3;
+ switch (a) {
+     case 3:
+         console.log('wrong answer');
+         break;
+         case 4:             console.log('close! but still the wrong answer');
+             break;
+             case 2:
+                 console.log('totally wrong.');
+                 break;
+                 case 5:
+                     console.log('right! you are good to go to the next stage');
+                     break;
+                  default:
+                       console.log('your values are out of this league');
+ }
