dee79bbea267d2267389d75593fd4f2485af43b5
yusuf ridwan <horiyorrmi72@gmal.com>
2020-11-11 09:11:39 +0100

    first commit

---
+++ learning.js
@@ -0,0 +1,69 @@
+// ...............do..while loop..........................
+// let i = 0;
+// do{
+//     console.log('welcome to our domain!');
+    // i++
+// } while(i < 20);
+// ....................************************...................
+
+//........................creating a loop for odd values or numbers...........
+
+// let i = 0;
+// for(i = 0; i < 10; i++){
+//     if(i % 2 === 0) continue;
+
+//     console.log(i);
+// }
+// ......................*****************************.........................
+
+// .....................creating a loop for even values..............
+
+// let i = 1;
+// for(i = 1; i < 10; i++){
+//     if(i % 2 === 1) continue;
+
+//     console.log(i);
+// }
+// ....................**********************...........................
+
+// ................... creating a switch method that has the correct value init.....................................
+// let a = 2+3;
+// switch (a) {
+//     case 3:
+//         console.log('wrong answer');
+//         break;
+//         case 4:
+//             console.log('close! but still the wrong answer');
+//             break;
+//             case 2:
+//                 console.log('totally wrong.');
+//                 break;
+//                 case 5:
+//                     console.log('right! you are good to go to the next stage');
+//                     break;
+//                     default:
+//                         console.log('your values are out of this league');
+
+// }
+// .................********************************************************.........................................
+
+// .....creating a switch method that execute the default code in absence of it true answer..........................
+
+// let a = 2+3;
+// switch (a) {
+//     case 3:
+//         console.log('wrong answer');
+//         break;
+//         case 4:
+//             console.log('close! but still the wrong answer');
+//             break;
+//             case 2:
+//                 console.log('totally wrong.');
+//                 break;
+//                 // case 5:
+//                 //     console.log('right! you are good to go to the next stage');
+//                 //     break;
+//                     default:
+//                         console.log('your value does not exist in this terminal (:');
+}
+// ....................***************************************************************.................................
